0c8b927e471421205b7ad6ceaa8464ceb6d027a6
BrisaBohle <81156231+BrisaBohle@users.noreply.github.com>
2021-04-13 10:25:02 +0200

    Interactive icon weather

--- src/script.js
+++ src/script.js
@@ -32,6 +32,13 @@ time.innerHTML = `${hours}:${minutes}`;
 function displayWeather(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   setTemperature(response.data.main.temp);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+  console.log(iconElement);
 }
 
 function searchCity(city) {
